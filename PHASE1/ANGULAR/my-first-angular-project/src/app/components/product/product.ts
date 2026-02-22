62bcfcfa20272d6ba769cf792a76bf9284cccfd3
aw1app <aw1app@outlook.com>
2026-02-22 22:24:02 +0530

    Angular @Input demo

--- PHASE1/ANGULAR/my-first-angular-project/src/app/components/product/product.ts
+++ PHASE1/ANGULAR/my-first-angular-project/src/app/components/product/product.ts
@@ -8,6 +8,8 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductComponent {
 
+  // we are using @Input here so that the properties values are set by the 
+  // parent component (app.html)
   @Input("name") name!: string;
   @Input("price") price!:number;
 
--- PHASE1/ANGULAR/my-first-angular-project/src/app/components/user/user.ts
+++ PHASE1/ANGULAR/my-first-angular-project/src/app/components/user/user.ts
@@ -9,6 +9,4 @@ import { Component } from '@angular/core';
 export class UserComponent {
   name: string = 'John';
   age: number = 25;
-
-
 }
